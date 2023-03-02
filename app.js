dc4bb7b3f27518e11cdaa6d778950cc374382ae7
vuxuantruong190325 <101324265+vuxuantruong190325@users.noreply.github.com>
2023-03-02 22:55:18 +0700

    update month

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ function updateTimer() {
 
     var timeDiff = end.getTime() - now.getTime();
 
+    var months = (end.getFullYear() - now.getFullYear()) * 12;
+    months -= now.getMonth() + 1;
+    months += end.getMonth() + 1;
     var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
     var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
@@ -13,7 +16,8 @@ function updateTimer() {
     var totalHours = days * 24 + hours;
     var totalMinutes = totalHours * 60 + minutes;
 
-    document.getElementById("timer").innerHTML = days + " ngày " + hours + " giờ " + minutes + " phút " + seconds + " giây ";
+    // document.getElementById("month").innerHTML = months + "tháng"
+    document.getElementById("timer").innerHTML = months + " tháng " + days + " ngày " + hours + " giờ " + minutes + " phút ";
     document.getElementById("total-hours").innerHTML = totalHours + " giờ " + minutes + " phút";
     document.getElementById("total-minutes").innerHTML = totalMinutes + " phút " + seconds + " giây";
 }
